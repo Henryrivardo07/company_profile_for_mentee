7ff7e72f30af12a4bb79597e0c0087244edc5032
Henry Rivardo <henrysihombing20@gmail.com>
2026-07-10 20:23:44 +0700

    section marquee logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./home/partials/hero";
 import Navbar from "./home/partials/navbar";
+import TrustedBy from "./home/partials/trusted-by";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <TrustedBy />
     </div>
   );
 }
